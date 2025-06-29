76c5b4c9d6c8114b1e18ef961611244f8ffc9d9c
josh-guarda <joshuaguarda23@gmail.com>
2025-06-29 22:12:57 +0800

    [Feat]password reset stage1

--- my_app/routes.py
+++ my_app/routes.py
@@ -108,108 +108,11 @@ def admin_dashboard():
         return redirect(url_for("home_page"))
     return render_template("admin/admin_dashboard.html",show_navbar=False)
     
-@app.route('/internal/')
-@login_required
+@app.route('/internal/') 
 def internal_dashboard():
     if current_user.user_type.name != "internal":
         return redirect(url_for("home_page"))
     return render_template('internal/internal_dashboard.html',show_navbar=False)
-
-
-
-# @app.route('/public/',methods=["GET","POST"])
-# @login_required
-# def public_dashboard():
-    
-#     # OTHER APPROACH
-#     form=PersonalInfoForm(obj=current_user)
-    
-    
-#     form.brgy_id.choices = [('','-- Select Barangay --')] + sorted(
-#         [(brgy['brgyCode'], brgy['brgyDesc']) for brgy in BARANGAY_DATA],
-#         key=lambda x: x[0].lower()  # Sort by barangay name (case-insensitive)
-#     )
-
-#     form.munci_id.choices = [('', '-- Select Municipality --')] + sorted(
-#         [(muni['citymunCode'], muni['citymunDesc']) for muni in MUNICIPALITY_DATA],
-#         key=lambda x: x[0].lower()  # Sort by municipality name
-#     )
-
-#     form.prov_id.choices = [('', '-- Select Province --')] + sorted(
-#         [(prov['provCode'], prov['provDesc']) for prov in PROVINCE_DATA],
-#         key=lambda x: x[0].lower()  # Sort by province name
-#     )
-
-    
-#     if form.validate_on_submit():
-        
-#         if form.cancel.data:
-#             flash('Update Canceled.', category='danger')
-#             return redirect(url_for('public_dashboard'))
-
-#         current_user.firstname = form.firstname.data
-#         current_user.lastname = form.lastname.data
-#         current_user.email_address = form.email_address.data
-#         current_user.mobile_number = form.mobile_number.data
-#         current_user.phone_number = form.phone_number.data
-#         current_user.address_1 = form.address_1.data
-#         current_user.address_2 = form.address_2.data
-        
-#         # current_user.brgy_id = form.brgy_id.data
-#         # current_user.brgy_name = form.brgy_name.data
-        
-#         current_user.brgy_id = form.brgy_id.data
-#         current_user.munci_id = form.munci_id.data
-#         current_user.prov_id = form.prov_id.data
-#         current_user.zipcode = form.zipcode.data
-        
-#         # Handle avatar upload
-#         if form.avatar.data:
-#             avatar_file = form.avatar.data
-#             filename = secure_filename(avatar_file.filename)
-#             ext = avatar_file.filename.rsplit('.', 1)[-1].lower()
-#             avatar_name = str(uuid.uuid4()) + "_" + filename
-#             upload_path = os.path.join(app.config['UPLOAD_FOLDER'],avatar_name)
-            
-#             try:
-#                 avatar_file.save(upload_path)
-#                 current_user.profile_picture = avatar_name
-
-#             except FileNotFoundError as e:
-#                 flash('directory cannot found!', category='danger')
-                
-            
-#         db.session.commit()
-#         flash('Your profile has been updated!', category='success')
-#         return redirect(url_for('public_dashboard'))
-    
-    
-#     if form.errors !={}: # if there is no errors from validations inside the forms.py
-#         for err_msg in form.errors.values():
-#             flash(f'There is some Error in {err_msg}',category='danger')
-    
-    
-#     if current_user.user_type.name != "public":
-#         return redirect(url_for("home_page"))
-#     return render_template('public/public_dashboard.html',show_navbar=False,form=form)
-
-
-
-
-# @app.route('/search/barangay')
-# def search_barangay():
-#     term = request.args.get('term', '').lower()
-#     with open(Barangay_JSON, encoding='utf-8') as f:
-#         records = json.load(f)['RECORDS']
-
-#     # Filter barangays by keyword
-#     matches = [
-#         {'id': brgy['brgyCode'], 'text': brgy['brgyDesc']}
-#         for brgy in records
-#         if term in brgy['brgyDesc'].lower()
-#     ]
-
-#     return jsonify(matches[:30])
 
 
 
