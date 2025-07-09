58a75dec81ef131dcf1fee88b89e45320a897d5e
josh-guarda <joshuaguarda23@gmail.com>
2025-07-09 23:48:36 +0800

    Users management responsiveness still fixing tool tip hamburger bug

--- my_app/static/main.js
+++ my_app/static/main.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    // TOAST MESSAGE
     const toastEl = document.getElementById("toastMessage");
     if (toastEl) {
         const toast = new bootstrap.Toast(toastEl);
         toast.show();
     }
-});
 
+    // TOOL TIP
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+      new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
 
 
 
